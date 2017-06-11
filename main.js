2a91819d683134ef9766717c2eacf91431e349c8
Jerrud Sabourin <jsabourin1235@gmail.com>
2017-06-11 10:57:31 -0700

    Updated JS without comments. #2

--- main.js
+++ main.js
@@ -2,10 +2,8 @@ var $quote = document.querySelector('#quote')
 
 var quote = '"Everybody wants happiness, nobody wants pain, but you can\'t have a rainbow without a little rain."'
 
-/*creates an array of each letter*/
 var letter = quote.split('')
 
-/*grabs each letter and creates a span from it*/
 for (var i = 0; i < letter.length; i++) {
   var el = document.createElement('span')
   el.textContent = letter[i]
