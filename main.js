7f5da8bc707fd9392058a905933fc41a5d497573
Jerrud Sabourin <jsabourin1235@gmail.com>
2017-06-12 14:49:28 -0700

    Attempting to fix merge conflict.

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
 var $quote = document.querySelector('#quote')
-
 var quote = '"Everybody wants happiness, nobody wants pain, but you can\'t have a rainbow without a little rain."'
-
 var letter = quote.split('')
+var score = 2
 
 for (var i = 0; i < letter.length; i++) {
   var el = document.createElement('span')
@@ -19,12 +18,42 @@ document.addEventListener('keypress', function(event) {
   var $current = document.querySelector('.current')
   if (event.key === $current.textContent) {
     var $next = $current.nextElementSibling
+<<<<<<< HEAD
+    if ($next === null) {
+      var $result = document.querySelector('#result')
+      $result.textContent = showScore(score)
+    }
+    else {
+      $current.classList.remove('current')
+      $current.classList.add('right')
+      $next.classList.add('current')
+      score += 1
+      return
+    }
+=======
     $current.classList.remove('current')
     $current.classList.add('right')
     $next.classList.add('current')
     return
+>>>>>>> master
   }
   else {
     $current.classList.add('wrong')
+    score -= 1
   }
 })
+<<<<<<< HEAD
+
+function showScore(score) {
+  if (score >= 90) {
+    return 'WOW! INCREDIBLE! You scored ' + score + '%'
+  }
+  else if (score >= 80) {
+    return 'Pretty good...but there\'s room for improvement here. You scored ' + score + '%'
+  }
+  else {
+    return 'Try again. You scored ' + score + '%'
+  }
+}
+=======
+>>>>>>> master
