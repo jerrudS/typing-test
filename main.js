d8360f20d089c323b21156592bb630477382be3a
Jerrud Sabourin <jsabourin1235@gmail.com>
2017-06-12 13:15:19 -0700

    Updated implementation section. #4

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
 var $quote = document.querySelector('#quote')
-
 var quote = '"Everybody wants happiness, nobody wants pain, but you can\'t have a rainbow without a little rain."'
-
 var letter = quote.split('')
+var score = 2
 
 for (var i = 0; i < letter.length; i++) {
   var el = document.createElement('span')
@@ -19,24 +18,32 @@ document.addEventListener('keypress', function(event) {
   var $current = document.querySelector('.current')
   if (event.key === $current.textContent) {
     var $next = $current.nextElementSibling
-    $current.classList.remove('current')
-    $current.classList.add('right')
-    $next.classList.add('current')
+    if ($next === null) {
+      var $result = document.querySelector('#result')
+      $result.textContent = showScore(score)
+    }
+    else {
+      $current.classList.remove('current')
+      $current.classList.add('right')
+      $next.classList.add('current')
+      score += 1
+      return
+    }
   }
   else {
-    $current.classList.remove('current')
     $current.classList.add('wrong')
+    score -= 1
   }
 })
 
-document.addEventListener('keypress', function(event) {
-  var $wrong = document.querySelector('.wrong')
-  var $nextWrong = $wrong.nextElementSibling
-  if (event.key === $wrong.textContent) {
-    $wrong.classList.remove('wrong')
-    $wrong.classList.add('right')
-    $nextWrong.classList.add('current')
+function showScore(score) {
+  if (score >= 90) {
+    return 'WOW! INCREDIBLE! You scored ' + score + '%'
+  }
+  else if (score >= 80) {
+    return 'Pretty good...but there\'s room for improvement here. You scored ' + score + '%'
   }
   else {
+    return 'Try again. You scored ' + score + '%'
   }
-})
+}
